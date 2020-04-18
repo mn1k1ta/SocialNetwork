237742e6ec1b4da3700919dc9751edafc75614a6
n1kem <mn1k1ta@users.noreply.github.com>
2020-04-18 06:34:53 +0300

    Finished UserProfileController

--- WebApplication2sasa/Controllers/UserProfileController.cs
+++ WebApplication2sasa/Controllers/UserProfileController.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using BLL.Interfaces;
+using BLL.ModelDTO;
+using BLL.Services.Filters;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.ViewModels;
 
 namespace WebApi.Controllers
 {
@@ -26,11 +29,44 @@ namespace WebApi.Controllers
 
 
         [HttpGet]
-        [Route("")]
+        [Route("GetAllUserProfile")]
         public async Task<IActionResult> GetAllUserProfile()
         {
             return Ok(await _userProfileService.GellAllUserProfileAsync());
         }
 
+        [HttpGet]
+        [Route("GetUserProfileByName")]
+        public async Task<IActionResult> GetUserProfileByName(string name)
+        {
+            return Ok(await _userProfileService.FindUserProfileByNameAsync(name));
+        }
+
+        [HttpGet]
+        [Route("GetUserProfileById")]
+        public async Task<IActionResult> GetUserProfileById(int id)
+        {
+            return Ok(await _userProfileService.FindUserProfileByIdAsync(id));
+        }
+
+        [HttpGet]
+        [Route("SearchByFilters")]
+        public async Task<IActionResult> FindUsers([FromQuery] FilterModel filterModel)
+        {
+            var filters = new List<IFilter>();
+            filters.Add(new NameFilter(filterModel.Name));
+            filters.Add(new GenderFilter(filterModel.Gender));
+            filters.Add(new CountryFilter(filterModel.Country));
+            filters.Add(new CityFilter(filterModel.City));
+            filters.Add(new AgeFilter(filterModel.MinAge, filterModel.MaxAge));
+            return Ok(await _userProfileService.FilterManagerAsync(filters.ToArray()));
+        }
+
+        [HttpPut]
+        [Route("EditUserProfile")]
+        public async Task<IActionResult> EditUserProfile(UserProfileDTO userProfileDTO)
+        {
+            return Ok(await _userProfileService.UpdateUserProfileAsync(userProfileDTO));
+        }
     }
 }
