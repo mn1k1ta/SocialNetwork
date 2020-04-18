bd807ba94e8f7470868af23fa7011807d04e5cac
n1kem <mn1k1ta@users.noreply.github.com>
2020-04-18 05:55:11 +0300

    Add UserProfileController

---
+++ WebApplication2sasa/Controllers/UserProfileController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "User")]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserProfileService _userProfileService;
+        private readonly IMapper _mapper;
+
+        public UserProfileController(IUserProfileService _userProfileService, IMapper _mapper)
+        {
+            this._mapper = _mapper;
+            this._userProfileService = _userProfileService;
+        }
+
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetAllUserProfile()
+        {
+            return Ok(await _userProfileService.GellAllUserProfileAsync());
+        }
+
+    }
+}
