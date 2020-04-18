fab7baf58f38e0ffd97acf7a8d47693cfd17e4a0
n1kem <mn1k1ta@users.noreply.github.com>
2020-04-18 05:40:34 +0300

    Revert "Revert "fix(Create userProfile)""
    This reverts commit d7b3ff981e6ea4ebe655c46820b0ced63587ff50.

--- WebApplication2sasa/ViewModels/LoginModel.cs
+++ WebApplication2sasa/ViewModels/LoginModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebApi.ViewModels
+﻿namespace WebApi.ViewModels
 {
     public class LoginModel
     {
